34527d3b94134658538b6688ac454a3c5b744784
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-09 14:33:00 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -19,6 +19,22 @@
    async function getData() {
 	const result = await fetch('https://randomuser.me/api?results=4')
 	const testimonials = await result.json();
+	console.log(testimonials)
+	testimonials.results.forEach(element => {
+	
+			const test_box = document.createRange().createContextualFragment(`
+				
+				 <div class="test-box-1">
+					 <img src="${element.picture.large}" alt="">
+					<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptates officiis earum ipsam velit odio est. Omnis illum quis dolorum!</p>
+					<h6>Monica I.</h6>
+				</div>
+	
+				`)
+				const testimonials_row = document.querySelector('.testimonials-row')
+				testimonials_row.append(test_box)
+		
+	});
    }
 
-   getData
+   getData()
