eb193bfe4bb2fd4707c03e823f0b1782688c9904
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-12 11:36:00 -0500

    first commit

--- js/script.js
+++ js/script.js
@@ -37,4 +37,24 @@
 	});
    }
 
+   const btn_validar = document.getElementById('btn-validar').onclick = (e) => {
+	e.preventDefault();
+	const nombre = document.getElementById('nombre');
+	const email = document.getElementById('email');
+	const fecha = document.getElementById('fecha');
+	const hora = document.getElementById('hora');
+	const mensaje = document.getElementById('mensaje');
+	const arr = [nombre, email, fecha, hora, mensaje];
+	const messageArr = ["Nombre", "Email", "Fecha", "Hora", "Mensaje"];
+	for(i = 0; i < arr.length; i++){
+		if(arr[i].value == ""){
+			swal({
+				title: `El campo ${messageArr[i]} no puede estar vacío`,
+				icon: "error",
+				 })
+				 return false;
+		}
+	}
+}
+
    getData()
